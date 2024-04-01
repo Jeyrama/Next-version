ad9c0f2ae9157ff75109285aff4ea00300e18c90
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-03-31 21:44:02 -0700

    Update Next-version.js

--- Next-version.js
+++ Next-version.js
@@ -23,3 +23,16 @@ Rules:
 
 
 // Solution
+
+function nextVersion(ver){
+  let arr = ver.split(".").map(el => parseInt(el));
+  for (let i = arr.length - 1; i >= 0; i--) {
+     if (arr[i] + 1 == 10 && i != 0) { 
+       arr[i] = 0; 
+     } else {
+       arr[i]++;
+       break;
+     }
+  }
+  return arr.join(".");
+}
